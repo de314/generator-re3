e8bf43824443e1ee653274695cbc67fa630b182a
David Esposito <david@espo.coffee>
2017-12-08 23:45:57 -0500

    Adding component subgenerator

---
+++ generators/component/index.js
@@ -0,0 +1,54 @@
+'use strict'
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
+const _ = require('lodash')
+
+module.exports = class extends Generator {
+  prompting() {
+    const that = this
+    const hocChoices = [
+      { name: 'Proptypes', value: 'proptypes', checked: false },
+      { name: 'Redux connect', value: 'redux', checked: true },
+      { name: 'With Auth', value: 'auth', checked: false },
+      { name: 'With Router', value: 'router', checked: false },
+    ]
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name? No not include a path or file extension?',
+        validate: val => !_.isNil(val.match(/^[A-Z][a-zA-Z0-9]+$/)),
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message:
+          'Where should the component be saved? This will be prefixed with "./src/components/"',
+        default: ({ name }) => _.camelCase(name),
+        validate: val => !_.isNil(val.match(/^(?:[a-z][a-zA-Z0-9]+\/)*[a-z][a-zA-Z0-9]+$/)),
+      },
+      {
+        type: 'checkbox',
+        name: 'hoc',
+        message: 'Which Component features should be added?',
+        choices: hocChoices,
+        store: true,
+      },
+    ]
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props
+      hocChoices.forEach(feature => (props[feature.value] = props.hoc.includes(feature.value)))
+    })
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('__component.tpl.js'),
+      this.destinationPath(`src/components/${this.props.location}/${this.props.name}.js`),
+      this.props
+    )
+  }
+}
---
+++ generators/component/templates/__component.tpl.js
@@ -0,0 +1,42 @@
+import React from 'react'
+<% if (proptypes) { -%>
+import PropTypes from 'prop-types'
+<% } -%>
+<% if (redux) { -%>
+import { } from 'rdx/selectors'
+import { } from 'rdx/actions'
+<% } -%>
+
+import { compose } from 'recompose'
+<% if (redux) { -%>
+import { connect } from 'react-redux'
+<% } -%>
+<% if (auth) { -%>
+import withAuth from 'hoc/withAuth'
+<% } -%>
+<% if (router) { -%>
+import { withRouter } from 'react-router-dom'
+<% } -%>
+
+const <%= name -%> = ({}) => {
+  return (
+    <div className="<%= name -%>">
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+<% if (proptypes) { -%>
+
+<%= name -%>.propTypes = {}
+<% } -%>
+
+export const Raw<%= name %> = <%= name %>
+
+export default compose(
+<% if(redux) { -%>
+  connect(
+    state => ({}),
+    dispatch => ({}),
+  ),
+<% } -%>
+)(<%= name -%>)
